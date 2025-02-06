e156572ecb7c6fb3a91a80c44b0ee1735d87771a
suzuki-yasuhiro <yasuhiro.suzuki@techouse.jp>
2025-02-06 11:12:16 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  get 'users/new'
+  root "static_pages#home"
+  get  "/help",    to: "static_pages#help"
+  get  "/signup",  to: "users#new"
 end
