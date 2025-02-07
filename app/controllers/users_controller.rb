8f913b06f1cb75a57f48cedf877477bf227812d9
suzuki-yasuhiro <yasuhiro.suzuki@techouse.jp>
2025-02-07 03:14:33 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_prams)
+    if @user.save
+      flash[:success] = "Created User Account Successfly"
+      redirect_to @user
+    else
+      render "new", status: :unprocessable_entity   # code 422
+    end
+  end
+
+  private
+
+    def user_prams
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    
 end
